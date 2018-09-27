c745bf55ffbf56571cc69206152868a22e633777
Scott Sweeney <treblecode@users.noreply.github.com>
2018-09-26 22:23:43 -0400

    adding child routes

--- src/build/PartSelector.vue
+++ src/build/PartSelector.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="part" :class="position">
-    <img :src="selectedPart.src" title="arm"/>
+    <router-link :to="{
+        name: 'Parts',
+        params: {
+          id: this.selectedPart.id,
+          partType: this.selectedPart.type,
+        }}">
+      <img  :src="selectedPart.src" title="arm"/>
+      </router-link>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
     <span class="sale" v-show="selectedPart.onSale">Sale!</span>
@@ -94,6 +101,7 @@ export default {
 }
 .part img {
   width:165px;
+  cursor: pointer;
 }
 .top {
   border-bottom: none;
---
+++ src/parts/PartInfo.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h1>{{ part.title }}</h1>
+        <div>
+            {{ part.description }}
+        </div>
+    </div>
+</template>
+
+<script>
+import parts from '../data/parts';
+
+export default {
+  name: 'PartInfo',
+  props: {
+    partType: {
+      type: String,
+    },
+    id: {
+      type: [Number, String],
+      validator(value) {
+        return Number.isInteger(Number(value));
+      },
+    },
+  },
+  computed: {
+    part() {
+      const { partType, id } = this;
+      // route ID's will always be numbers so id must be cast to an int via +
+      return parts[partType].find(part => part.id === +id);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
